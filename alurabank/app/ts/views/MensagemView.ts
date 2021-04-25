c0e0b2d667240055050a4474a035639c1b5bec45
Carlos Vinicios de F Fernandes <carlos.vinicios.ff@gmail.com>
2021-04-25 18:02:44 -0300

    utilizando heranca nas views

--- alurabank/app/ts/views/MensagemView.ts
+++ alurabank/app/ts/views/MensagemView.ts
@@ -1,16 +1,10 @@
-class MensagemView {
+class MensagemView extends View{
 
-    private _elemento: Element;
-
-    constructor(seletor: string) {
-        this._elemento = document.querySelector(seletor);
-    }
-
-    update(model:string):void {
+    update(model:string): void {
         this._elemento.innerHTML = this.template(model);
     }
 
-    private template(model: string): string {
+    template(model: string): string {
         return `<p class="alert alert-info">${model}</p>`;
     }
 }
